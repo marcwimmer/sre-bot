da8e1cd991b0211136f2ca76ea793f1dc4433e4a
eliwilbert <eliwilbert@github.com>
2023-07-10 09:50:34 +0200

    wheel error test

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ import sys
 import subprocess
 from shutil import rmtree
 from pathlib import Path
+import wheel
 
 try:
     import setuptools
@@ -37,7 +38,7 @@ metadata = config["metadata"]
 required_version = (60, 5, 0)
 setuptools_version = tuple(map(int, setuptools.__version__.split(".")))
 setuptools_version = (61, 0, 0)
-print(setuptools_version)
+#print(setuptools_version)
 if setuptools_version < required_version:
     raise Exception(f"Requires setup version {'.'.join(map(str, required_version))}")
 
